6c8b9914811251f3d091e70d89e419cb4f5d4b7a
PAbleBoo <baalavignesh21@gmail.com>
2021-03-07 18:45:22 +0530

    second

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 
-var serviceAccount = require("./permission.json");
+// var serviceAccount = require("./permission.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-const express = require("express");
-const app = express()
+// const express = require("express");
+// const app = express()
 
-const cors = require('cors');
-app.use( cors( { origin:true } ))
+// const cors = require('cors');
+// app.use( cors( { origin:true } ))
 
 
 // Routes
@@ -27,150 +27,150 @@ app.get('/hello', (req, res) => {
 
 
 
-// POST - CREATE DATA
-app.post('/api/create', (req, res) => {
+// // POST - CREATE DATA
+// app.post('/api/create', (req, res) => {
 
-    (async () => {
-        try {
-            const ref = db.collection("products").doc();
-            let id = ref.id;
-            console.log(id);
-            console.log('create');
-            await db.collection('products').doc(id).create({
-                p_id: id,
-                p_name: req.body.name,
-                p_type: req.body.type,
-                p_anime: req.body.anime,
-                p_description: req.body.description,
-                p_price: req.body.price
-            })
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-
-
-    return res.status(200).send('posting data');
-})
-
-// GET - GET DATA
+//     (async () => {
+//         try {
+//             const ref = db.collection("products").doc();
+//             let id = ref.id;
+//             console.log(id);
+//             console.log('create');
+//             await db.collection('products').doc(id).create({
+//                 p_id: id,
+//                 p_name: req.body.name,
+//                 p_type: req.body.type,
+//                 p_anime: req.body.anime,
+//                 p_description: req.body.description,
+//                 p_price: req.body.price
+//             })
+//         }
+//         catch(error) {
+//             console.log(error);
+//             return res.status(500).send(error);
+//         }
+//     })();
 
 
-app.get('/api/product/:id', (req, res) => {
-    (async () => {
-        try {
-            console.log('getting data')
-            const document = db.collection("products").doc(req.params.id);
-            let product = await document.get();
-            let response = product.data();
-            return res.status(200).send(response);
-        }
+//     return res.status(200).send('posting data');
+// })
+
+// // GET - GET DATA
 
 
-        catch(error) {
-            res.status(500).send(error);
-        }
-    })();
-})
-
-// GET - GET ALL DATA
-
-
-app.get('/api/product', (req, res) => {
-    (async () => {
-        try {
-            console.log('getting all data')
-            let query = db.collection("products");
-            let response = [];
-
-            await query.get().then(querySnapshot => {
-                let docs = querySnapshot.docs;
-
-                for (let doc of docs) {
-                    console.log(doc);
-
-                    let product = {
-                        "id" : doc.id,
-                        "name" :doc.data().p_name,
-                        "type" : doc.data().p_type,
-                        "description" : doc.data().p_description,
-                        "price" : doc.data().p_price,
-                        "anime" : doc.data().p_anime,
-                    }
-
-                    response.push(product);
-                }
-                return response;
-            });
-            return res.status(200).send(response);
-        }
+// app.get('/api/product/:id', (req, res) => {
+//     (async () => {
+//         try {
+//             console.log('getting data')
+//             const document = db.collection("products").doc(req.params.id);
+//             let product = await document.get();
+//             let response = product.data();
+//             return res.status(200).send(response);
+//         }
 
 
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-})
+//         catch(error) {
+//             res.status(500).send(error);
+//         }
+//     })();
+// })
+
+// // GET - GET ALL DATA
 
 
-// PUT - UPDATE DATA
+// app.get('/api/product', (req, res) => {
+//     (async () => {
+//         try {
+//             console.log('getting all data')
+//             let query = db.collection("products");
+//             let response = [];
+
+//             await query.get().then(querySnapshot => {
+//                 let docs = querySnapshot.docs;
+
+//                 for (let doc of docs) {
+//                     console.log(doc);
+
+//                     let product = {
+//                         "id" : doc.id,
+//                         "name" :doc.data().p_name,
+//                         "type" : doc.data().p_type,
+//                         "description" : doc.data().p_description,
+//                         "price" : doc.data().p_price,
+//                         "anime" : doc.data().p_anime,
+//                     }
+
+//                     response.push(product);
+//                 }
+//                 return response;
+//             });
+//             return res.status(200).send(response);
+//         }
 
 
-app.put('/api/update/:id', (req, res) => {
-
-    (async () => {
-        try {
-            console.log('updating data');
-            const document = db.collection('products').doc(req.params.id);
-
-            await document.update({
-                "p_name": req.body.name,
-                "p_type": req.body.type,
-                "p_description": req.body.description,
-                "p_price": req.body.price,
-                "p_anime": req.body.anime
-            });
-
-            res.status(200).send();
-
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
+//         catch(error) {
+//             console.log(error);
+//             return res.status(500).send(error);
+//         }
+//     })();
+// })
 
 
-    return res.status(200).send('updating data');
-})
+// // PUT - UPDATE DATA
 
 
-// DELETE - DELETE DATA
+// app.put('/api/update/:id', (req, res) => {
+
+//     (async () => {
+//         try {
+//             console.log('updating data');
+//             const document = db.collection('products').doc(req.params.id);
+
+//             await document.update({
+//                 "p_name": req.body.name,
+//                 "p_type": req.body.type,
+//                 "p_description": req.body.description,
+//                 "p_price": req.body.price,
+//                 "p_anime": req.body.anime
+//             });
+
+//             res.status(200).send();
+
+//         }
+//         catch(error) {
+//             console.log(error);
+//             return res.status(500).send(error);
+//         }
+//     })();
 
 
-app.delete('/api/delete/:id', (req, res) => {
-
-    (async () => {
-        try {
-            console.log('deleting data');
-            const document = db.collection('products').doc(req.params.id);
-
-            await document.delete();
-            res.status(200).send();
-
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
+//     return res.status(200).send('updating data');
+// })
 
 
-    return res.status(200).send('deleting data');
-})
+// // DELETE - DELETE DATA
+
+
+// app.delete('/api/delete/:id', (req, res) => {
+
+//     (async () => {
+//         try {
+//             console.log('deleting data');
+//             const document = db.collection('products').doc(req.params.id);
+
+//             await document.delete();
+//             res.status(200).send();
+
+//         }
+//         catch(error) {
+//             console.log(error);
+//             return res.status(500).send(error);
+//         }
+//     })();
+
+
+//     return res.status(200).send('deleting data');
+// })
 
 
 const port = process.env.PORT || 3000;
