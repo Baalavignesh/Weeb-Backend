c84ec9e141fad09df754674932214d5b4e2401c8
PAbleBoo <baalavignesh21@gmail.com>
2021-03-15 22:06:50 +0530

    for mothil

--- index.js
+++ index.js
@@ -1,178 +1,242 @@
 const admin = require("firebase-admin");
-
 var serviceAccount = require("./permission.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 
 const express = require("express");
-const app = express()
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const firebase = require("firebase");
+const app = express();
 
-const cors = require('cors');
-app.use( cors( { origin:true } ))
+// Setting Body Parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+// Setting up EJS
+app.set("view engine", "ejs");
+
+// Setting up CORS
+app.use(bodyParser({ limit: "50mb" }), cors({ origin: true }));
 
 // Routes
-app.get('/', (req, res) => {
-    return res.status(200).send('home page');
-})
+app.get("/", (req, res) => {
+  return res.render("home");
+  // return res.status(200).send('home page');
+});
 
-app.get('/hello', (req, res) => {
-    return res.status(200).send('working');
-})
+app.get("/hello", (req, res) => {
+  return res.status(200).send("working");
+});
 
+// GET - GET DATA OF SINGLE PRODUCT WITH ID - API
 
+app.get("/api/product/:id", (req, res) => {
+  (async () => {
+    try {
+      console.log("getting data");
+      const document = db.collection("products").doc(req.params.id);
+      let product = await document.get();
+      let response = product.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  })();
+});
 
-// POST - CREATE DATA
-app.post('/api/create', (req, res) => {
+// GET - GET ALL DATA - API
 
-    (async () => {
-        try {
-            const ref = db.collection("products").doc();
-            let id = ref.id;
-            console.log(id);
-            console.log('create');
-            await db.collection('products').doc(id).create({
-                p_id: id,
-                p_name: req.body.name,
-                p_type: req.body.type,
-                p_anime: req.body.anime,
-                p_description: req.body.description,
-                p_price: req.body.price
-            })
+app.get("/api/product", (req, res) => {
+  (async () => {
+    try {
+      console.log("getting all data");
+      let query = db.collection("products");
+      let response = [];
+
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+
+        for (let doc of docs) {
+          // console.log(doc);
+
+          let product = {
+            id: doc.id,
+            name: doc.data().p_name,
+            type: doc.data().p_type,
+            description: doc.data().p_description,
+            price: doc.data().p_price,
+            anime: doc.data().p_anime,
+          };
+
+          response.push(product);
         }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
+        return response;
+      });
+      return res.status(200).send(response);
+      // res.render('all_products', {data: response});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+
+
+// GET ALL DATA FOR UI
+
+app.get("/api/allproduct", (req, res) => {
+  (async () => {
+    try {
+      console.log("getting all data");
+      let query = db.collection("products");
+      let response = [];
+
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+
+        for (let doc of docs) {
+          // console.log(doc);
+
+          let product = {
+            id: doc.id,
+            name: doc.data().p_name,
+            type: doc.data().p_type,
+            description: doc.data().p_description,
+            price: doc.data().p_price,
+            anime: doc.data().p_anime,
+          };
+
+          response.push(product);
         }
-    })();
+        return response;
+      });
+      // return res.status(200).send(response);
+      res.render("all_products", { data: response });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
 
-    return res.status(200).send('posting data');
-})
-
-// GET - GET DATA
-
-
-app.get('/api/product/:id', (req, res) => {
-    (async () => {
-        try {
-            console.log('getting data')
-            const document = db.collection("products").doc(req.params.id);
-            let product = await document.get();
-            let response = product.data();
-            return res.status(200).send(response);
-        }
+// ***************************************************************************
+// This is the Post API part
+// ***************************************************************************
 
 
-        catch(error) {
-            res.status(500).send(error);
-        }
-    })();
-})
+// GET AND POST - CREATE DATA
 
-// GET - GET ALL DATA
+// To render the create screen
+app.get("/api/create", (req, res) => {
+  return res.render("add_product");
+});
 
+// This is the main function we are working on right now
+app.post("/api/create", urlencodedParser, (req, res) => {
+  (async () => {
+    try {
+      // We use req.body.name of the input tag in add_product.jsx input tags
+      console.log(req.body.name);
 
-app.get('/api/product', (req, res) => {
-    (async () => {
-        try {
-            console.log('getting all data')
-            let query = db.collection("products");
-            let response = [];
+      // This is not working for me. Firebase storage
+      var storage = firebase.storage();
+      var storageRef = storage.ref();
 
-            await query.get().then(querySnapshot => {
-                let docs = querySnapshot.docs;
+      // This is uploadding the data to firebase, normal data. This is working fine
+      const ref = db.collection("products").doc();
+      let id = ref.id;
+      console.log("create");
+      await db.collection("products").doc(id).create({
+        p_id: id,
+        p_name: req.body.name,
+        p_type: req.body.type,
+        p_anime: req.body.anime,
+        p_description: req.body.description,
+        p_price: req.body.price,
+      });
+      res.render("home");
+    } catch (error) {
+      console.log("error here");
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
-                for (let doc of docs) {
-                    console.log(doc);
+// PUT - UPDATE DATA (IGNORE THIS)
 
-                    let product = {
-                        "id" : doc.id,
-                        "name" :doc.data().p_name,
-                        "type" : doc.data().p_type,
-                        "description" : doc.data().p_description,
-                        "price" : doc.data().p_price,
-                        "anime" : doc.data().p_anime,
-                    }
+app.put("/api/update/:id", (req, res) => {
+  (async () => {
+    try {
+      console.log("updating data");
+      const document = db.collection("products").doc(req.params.id);
 
-                    response.push(product);
-                }
-                return response;
-            });
-            return res.status(200).send(response);
-        }
+      await document.update({
+        p_name: req.body.name,
+        p_type: req.body.type,
+        p_description: req.body.description,
+        p_price: req.body.price,
+        p_anime: req.body.anime,
+      });
 
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-})
+  return res.status(200).send("updating data");
+});
 
+// DELETE - DELETE DATA (IGNORE THIS)
 
-// PUT - UPDATE DATA
+app.delete("/api/delete/:id", (req, res) => {
+  (async () => {
+    try {
+      console.log("deleting data");
+      const document = db.collection("products").doc(req.params.id);
 
+      await document.delete();
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 
-app.put('/api/update/:id', (req, res) => {
-
-    (async () => {
-        try {
-            console.log('updating data');
-            const document = db.collection('products').doc(req.params.id);
-
-            await document.update({
-                "p_name": req.body.name,
-                "p_type": req.body.type,
-                "p_description": req.body.description,
-                "p_price": req.body.price,
-                "p_anime": req.body.anime
-            });
-
-            res.status(200).send();
-
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-
-
-    return res.status(200).send('updating data');
-})
-
-
-// DELETE - DELETE DATA
-
-
-app.delete('/api/delete/:id', (req, res) => {
-
-    (async () => {
-        try {
-            console.log('deleting data');
-            const document = db.collection('products').doc(req.params.id);
-
-            await document.delete();
-            res.status(200).send();
-
-        }
-        catch(error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-
-
-    return res.status(200).send('deleting data');
-})
-
+  return res.status(200).send("deleting data");
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening to port ${port}`))
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
 // exports.app = functions.https.onRequest(app);
+
+// UNWANTED
+
+// const gc = new Storage({
+//     keyFilename: path.join(__dirname, "./permission.json"),
+//     projectId: "weeb-store-b5e25",
+//   });
+
+// gc.getBuckets().then((x) => console.log(x));
+
+// const weebBucket = gc.bucket("weeb-store-b5e25.appspot.com");
+//   var storageRef = storage.storage({
+//     projectId: "weeb-store-b5e25",
+//     keyFilename: "/permission.json",
+//   });
+
+//   const bucket = storage.bucket("weeb-store-b5e25.appspot.com");
+
+//   const [url] = await storage
+//     .bucket(bucketName)
+//     .file(filename)
+//     .getSignedUrl(options);
